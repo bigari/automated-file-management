786cb3da7c7668c228503c3a1186fee783b7d6c4
bigari <bigari7@gmail.com>
2019-10-30 16:04:36 +0100

    Used postgres in a container as db

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const db = require('./server/models/index');
 
 // Set up the express app
 const app = express();
