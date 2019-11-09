c80a68d56a54726c76cfd317572aeeaf5a33f5bb
bigari <bigari7@gmail.com>
2019-11-09 17:47:29 +0100

    Made passport able to retrieve jwt either from cookie or authorization header

--- backend/passport.js
+++ backend/passport.js
@@ -1,36 +1,43 @@
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const jwtSecret = process.env['JWT_SECRET'];
-const {User} = require('./database/models/index');
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const jwtSecret = process.env["JWT_SECRET"];
+const { User } = require("./database/models/index");
 
-
-
-const cookieExtractor = function(req) {
-    var token = null;
-    if (req && req.cookies)
-    {
-        token = req.cookies['jwt'];
+const tokenExtractor = function(req) {
+  //token will be extracted first from cookie if possible
+  //If token not in cookie then we'll look in header
+  var token = null;
+  if (req) {
+    if (req.cookies) {
+      token = req.cookies["jwt"];
     }
-    return token;
+    if (!token && req.headers && req.headers.authorization) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+  }
+
+  return token;
 };
 
-passport.use(new JwtStrategy({
-    jwtFromRequest: cookieExtractor,
-    secretOrKey: jwtSecret,
-    passReqToCallback: true
-}, async (payload, done) => {
-    try{
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: tokenExtractor,
+      secretOrKey: jwtSecret
+    },
+    async (payload, done) => {
+      try {
         // Find the user specified in the token
-        const user = await User.findOne({ where : { id: payload.sub } });
-        if(!user) {
-            return done(null, false);
+        const user = await User.findOne({ where: { id: payload.sub } });
+        if (!user) {
+          return done(null, false);
         }
         done(null, user);
-
-    }catch(error){
-        done(error, false)
+      } catch (error) {
+        done(error, false);
+      }
     }
-}));
+  )
+);
 
 module.exports = passport;
