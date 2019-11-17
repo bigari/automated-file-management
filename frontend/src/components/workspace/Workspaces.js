164ca46008982129154f1e6d7c4e8b7f0862944e
bigari <bigari7@gmail.com>
2019-11-17 23:30:09 +0100

    Fetch workspaces from api in workspaces component

--- frontend/src/components/workspace/Workspaces.js
+++ frontend/src/components/workspace/Workspaces.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import WorkspaceList from './WorkspaceList';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import NavBar from '../NavBar';
+import React, { useEffect } from "react";
+import WorkspaceList from "./WorkspaceList";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import NavBar from "../NavBar";
+import { observer } from "mobx-react";
 
 const useStyles = makeStyles(theme => ({
-    container: {
-      marginTop: '20px',
-      marginBottom: '20px'
-    }
+  container: {
+    marginTop: "20px",
+    marginBottom: "20px"
+  }
 }));
-  
-const Workspaces = (props) => {
+
+const Workspaces = observer(props => {
   const userStore = props.userStore;
-  const workspaces = [
-    {id: 1, name: 'name 1'}, 
-    {id: 2, name: 'name 2'}, 
-    {id: 3, name: 'name 2'}, 
-    {id: 4, name: 'name 2'}, 
-    {id: 5, name: 'name 2'}, 
-  ];
+  const workspaceStore = props.workspaceStore;
+
+  useEffect(() => {
+    async function fetchWorkspaces() {
+      await workspaceStore.fetchAll();
+      // console.log(workspaceStore.list);
+    }
+    fetchWorkspaces();
+  }, [workspaceStore]);
+
+  const workspaces = workspaceStore.list;
   const classes = useStyles();
-    
+
   return (
     <div>
       <NavBar userStore={userStore} />
       <Container className="container">
         <Grid container>
-            <Typography variant="h6" className={classes.header}>
-                Workspaces
-            </Typography>
-            <WorkspaceList workspaces={workspaces} />
+          <Typography variant="h6" className={classes.header}>
+            Workspaces
+          </Typography>
+          <WorkspaceList workspaces={workspaces} />
         </Grid>
       </Container>
     </div>
-  )
-};
+  );
+});
 
 export default Workspaces;
