8f1684cd40e970fea8bdda7108288356e44bfd62
bigari <bigari7@gmail.com>
2019-11-17 23:26:34 +0100

    Created workspaceStore

---
+++ frontend/src/repositories/mobx/workspace/WorkspaceStore.js
@@ -0,0 +1,48 @@
+import { observable, action, computed, decorate, runInAction } from "mobx";
+import client from "../../client";
+
+export class WorkspaceStore {
+  workspaces;
+  isLoading;
+  root;
+  constructor(root) {
+    this.root = root;
+    this.workspaces = {};
+    this.isLoading = {
+      all: true
+    };
+  }
+
+  get count() {
+    return this.workspaces.length;
+  }
+  get list() {
+    return Object.values(this.workspaces);
+  }
+  fetchAll() {
+    if (
+      Object.keys(this.workspaces).length === 0 &&
+      this.workspaces.constructor === Object
+    ) {
+      return client.api
+        .url("/workspaces")
+        .get()
+        .json(json => {
+          runInAction(() => {
+            this.workspaces = {};
+            json.workspaces.forEach(
+              workspace => (this.workspaces[workspace.id] = workspace)
+            );
+            this.isLoading.all = false;
+          });
+        });
+    }
+  }
+}
+
+decorate(WorkspaceStore, {
+  workspaces: observable,
+  fetchAll: action,
+  count: computed,
+  list: computed
+});
