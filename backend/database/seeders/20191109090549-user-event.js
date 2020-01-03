b182a6f8ec8ed4adbe2f3347c66d58fe3039ee83
Rahal Hamza <hamzarahal031@gmail.com>
2020-01-03 11:05:03 +0100

    updated seeder

--- backend/database/seeders/20191109090549-user-event.js
+++ backend/database/seeders/20191109090549-user-event.js
@@ -4,6 +4,7 @@ const models = require("../models");
 const User = models.User;
 const Event = models.Event;
 const Question = models.Question;
+const Reply = models.Reply;
 
 const bcrypt = require("bcrypt");
 
@@ -24,7 +25,7 @@ module.exports = {
           email: "bigari7+afm1@gmail.com",
           password: bcrypt.hashSync("000000", salt),
           createdAt: new Date()
-        }
+        },
         // ,
         // {
         //   username: "steeve",
@@ -38,12 +39,12 @@ module.exports = {
         //   password: bcrypt.hashSync("000000", salt),
         //   createdAt: new Date()
         // },
-        // {
-        //   username: "test",
-        //   email: "test@gmail.com",
-        //   password: bcrypt.hashSync("test", salt),
-        //   createdAt: new Date()
-        // }
+        {
+          username: "test",
+          email: "test@gmail.com",
+          password: bcrypt.hashSync("test", salt),
+          createdAt: new Date()
+        }
       ],
       {}
     );
@@ -52,7 +53,7 @@ module.exports = {
 
     const users = await User.findAll({
       attributes: ["id"]
-    });
+    }).map(user => user.dataValues.id);
 
     console.log("seeding events");
     const now = new Date();
@@ -61,14 +62,14 @@ module.exports = {
       [
         {
           name: "The future of AI",
-          ownerId: users[0].id,
+          ownerId: users[0],
           accessCode: "00001G",
           startAt: now,
           endAt: new Date(new Date().setMonth(now.getMonth() + 1))
         },
         {
           name: "Robotic & design",
-          ownerId: users[0].id,
+          ownerId: users[0],
           accessCode: "00007Q",
           startAt: new Date(new Date().setMonth(now.getMonth() - 1)),
           endAt: now
@@ -81,7 +82,7 @@ module.exports = {
     
     const eids = await Event.findAll({
       attributes: ['id']  
-    });
+    }).map(event => event.dataValues.id);
 
     await queryInterface.bulkInsert(
       "Questions",
@@ -89,22 +90,43 @@ module.exports = {
         {
           content: "This is a question",
           timestamp: now,
-          eid: eids[0].id,
-          reply: "This is a reply"
+          eid: eids[0]
         },
         {
           content: "This is also a question",
           timestamp: now,
-          eid: eids[0].id,
-          reply: "This is a reply"
+          eid: eids[0]
         }
       ]
     );
+
+    console.log("Seeding replies")
+
+    const qids = await Question.findAll({
+      attributes: ['id']
+    }).map(question => question.dataValues.id);
+
+    await queryInterface.bulkInsert(
+      "Replies",
+      [
+        {
+          content: "This is a reply for question 2",
+          timestamp: new Date(),
+          qid: qids[0]
+        },
+        {
+          content: "This is a reply for question 1",
+          timestamp: new Date(),
+          qid: qids[1]
+        } 
+      ]
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("Events", null, {});
+    await queryInterface.bulkDelete("Replies", null, {});
     await queryInterface.bulkDelete("Questions", null, {});
+    await queryInterface.bulkDelete("Events", null, {});
     return queryInterface.bulkDelete("Users", null, {});
   }
 };
